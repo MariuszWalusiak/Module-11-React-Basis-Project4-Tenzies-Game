5cec3d31761ce67c560db02a8246721a99982f89
Mariusz Walusiak <marwal91@o2.pl>
2022-01-31 20:57:30 +0100

    Generate array of 10 random numbers

--- tenzies/src/App.js
+++ tenzies/src/App.js
@@ -1,20 +1,29 @@
 import Die from "./components/Die";
 
+const allNewDice = () => {
+  const diceArray=[]
+  for(let i=0; i < 10; i++) {
+    diceArray.push(Math.ceil(Math.random() * 6))
+  }
+  return diceArray
+} 
+console.log(allNewDice())
+
 
 function App() {
   return (
       <main>
         <div className="grid-container">
-          <Die value={Math.floor(Math.random() * 6) + 1} />
-          <Die value={Math.floor(Math.random() * 6) + 1} />
-          <Die value={Math.floor(Math.random() * 6) + 1}/>
-          <Die value={Math.floor(Math.random() * 6) + 1}/>
-          <Die value={Math.floor(Math.random() * 6) + 1}/>
-          <Die value={Math.floor(Math.random() * 6) + 1}/>
-          <Die value={Math.floor(Math.random() * 6) + 1}/>
-          <Die value={Math.floor(Math.random() * 6) + 1} />
-          <Die value={Math.floor(Math.random() * 6) + 1}/>
-          <Die value={Math.floor(Math.random() * 6) + 1}/>
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1}/>
+          <Die value={1}/>
+          <Die value={1}/>
+          <Die value={1}/>
+          <Die value={1}/>
+          <Die value={1} />
+          <Die value={1}/>
+          <Die value={1}/>
         </div>
       </main>
   );
