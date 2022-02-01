350bb3f01f18a456ff532bf344f6a76d2fe7aab2
Mariusz Walusiak <marwal91@o2.pl>
2022-02-01 19:06:29 +0100

    hold dice part 2

--- tenzies/src/App.js
+++ tenzies/src/App.js
@@ -1,48 +1,52 @@
 import { useState } from "react";
 import Die from "./components/Die";
-import {nanoid} from "nanoid"
-
+import { nanoid } from "nanoid";
 
 function App() {
-
-  
   const allNewDice = () => {
-    const diceArray=[]
-    for(let i=0; i < 10; i++) {
+    const diceArray = [];
+    for (let i = 0; i < 10; i++) {
       diceArray.push({
-         value: Math.ceil(Math.random() * 6),
-         isHeld: false,
-         id:nanoid()
-        })
-      
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid(),
+      });
     }
-    
-    return diceArray
-  } 
-  
-  const [dice, setDice] = useState(allNewDice())
-  
-  const handleClickRollDice = () =>{
-    setDice(allNewDice())
-  }
+
+    return diceArray;
+  };
+
+  const [dice, setDice] = useState(allNewDice());
+
+  const handleClickRollDice = () => {
+    setDice(allNewDice());
+  };
 
   const holdDice = (id) => {
-    console.log(id)
-  }
-  
-  
-  const diceElements = dice.map(die => <Die value={die.value} key={die.id} isHeld={die.isHeld} holdDice={() => holdDice(die.id)}/>)
+    setDice((preDice) =>
+      preDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      })
+    );
+  };
 
-  
+  const diceElements = dice.map((die) => (
+    <Die
+      value={die.value}
+      key={die.id}
+      isHeld={die.isHeld}
+      holdDice={() => holdDice(die.id)}
+    />
+  ));
+
   return (
-      <main>
-        <div className="grid-container">
-          {diceElements}
-        </div>
-        <button className="roll-dice" onClick={handleClickRollDice}>Roll</button>
-      </main>
+    <main>
+      <div className="grid-container">{diceElements}</div>
+      <button className="roll-dice" onClick={handleClickRollDice}>
+        Roll
+      </button>
+    </main>
   );
 }
 
 export default App;
-
